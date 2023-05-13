ff29ed6e2ca939bed4db1662baafeb89363e3ed1
nyxb <contact@nyxb.xyz>
2023-05-13 12:37:38 +0200

    🎉 feat(esbuild.ts): add esbuild plugin to transpile TypeScript and JavaScript files 🎉 feat(json.ts): add json plugin to handle JSON files 🎉 feat(raw.ts): add raw plugin to handle raw files The esbuild plugin transpiles TypeScript and JavaScript files using the esbuild library. The json plugin handles JSON files and the raw plugin handles raw files such as markdown, text, css, html, and htm files. These plugins are added to the build process to improve the build pipeline and make it more efficient.

---
+++ src/builder/plugins/json.ts
@@ -0,0 +1,27 @@
+import type { Plugin } from 'rollup'
+import type { RollupJsonOptions } from '@rollup/plugin-json'
+import rollupJSONPlugin from '@rollup/plugin-json'
+
+const EXPORT_DEFAULT = 'export default '
+
+export function JSONPlugin(options: RollupJsonOptions): Plugin {
+   const plugin = rollupJSONPlugin(options)
+   return <Plugin>{
+      ...plugin,
+      name: 'buildkarium-json',
+      transform(code, id) {
+         // @ts-expect-error is not assignable to type
+         const res = plugin.transform!.call(this, code, id)
+         if (
+            res
+        && typeof res !== 'string'
+        && 'code' in res
+        && res.code
+        && res.code.startsWith(EXPORT_DEFAULT)
+         )
+            res.code = res.code.replace(EXPORT_DEFAULT, 'module.exports = ')
+
+         return res
+      },
+   }
+}
