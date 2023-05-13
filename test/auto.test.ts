2e52652c92b9a8c0d88ef0593d37aeb92b2e3b7c
nyxb <contact@nyxb.xyz>
2023-05-13 13:33:42 +0200

    🆕(auto.test.ts utils.test.ts): add tests

--- test/auto.test.ts
+++ test/auto.test.ts
@@ -1,257 +1,257 @@
-import { describe, it, expect } from "vitest";
-import { inferEntries, getEntrypointPaths } from "../src/auto";
+import { describe, expect, it } from 'vitest'
+import { getEntrypointPaths, inferEntries } from '../src/auto'
 
-describe("inferEntries", () => {
-  it("recognises main and module outputs", () => {
-    const result = inferEntries(
-      { main: "dist/test.cjs", module: "dist/test.mjs" },
-      ["src/", "src/test.ts"]
-    );
-    expect(result).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [{ input: "src/test" }],
-      warnings: [],
-    });
-  });
+describe('inferEntries', () => {
+   it('recognises main and module outputs', () => {
+      const result = inferEntries(
+         { main: 'dist/test.cjs', module: 'dist/test.mjs' },
+         ['src/', 'src/test.ts'],
+      )
+      expect(result).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [{ input: 'src/test' }],
+         warnings: [],
+      })
+   })
 
-  it("handles nested indexes", () => {
-    const result = inferEntries({ module: "dist/index.mjs" }, [
-      "src/",
-      "src/event/index.ts",
-      "src/index.ts",
-    ]);
-    expect(result).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [{ input: "src/index" }],
-      warnings: [],
-    });
-  });
-
-  it("handles binary outputs", () => {
-    expect(
-      inferEntries({ bin: "dist/cli.cjs" }, ["src/", "src/cli.ts"])
-    ).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [{ input: "src/cli" }],
-      warnings: [],
-    });
-    expect(
-      inferEntries({ bin: { nuxt: "dist/cli.js" } }, ["src/", "src/cli.ts"])
-    ).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [{ input: "src/cli" }],
-      warnings: [],
-    });
-    expect(
-      inferEntries({ bin: { nuxt: "dist/cli.js" }, type: "module" }, [
-        "src/",
-        "src/cli.ts",
+   it('handles nested indexes', () => {
+      const result = inferEntries({ module: 'dist/index.mjs' }, [
+         'src/',
+         'src/event/index.ts',
+         'src/index.ts',
       ])
-    ).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [{ input: "src/cli" }],
-      warnings: [],
-    });
-  });
+      expect(result).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [{ input: 'src/index' }],
+         warnings: [],
+      })
+   })
 
-  it("recognises `type: module` projects", () => {
-    const result = inferEntries({ main: "dist/test.js", type: "module" }, [
-      "src/",
-      "src/test.ts",
-    ]);
-    expect(result).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [{ input: "src/test" }],
-      warnings: [],
-    });
-  });
+   it('handles binary outputs', () => {
+      expect(
+         inferEntries({ bin: 'dist/cli.cjs' }, ['src/', 'src/cli.ts']),
+      ).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [{ input: 'src/cli' }],
+         warnings: [],
+      })
+      expect(
+         inferEntries({ bin: { nuxt: 'dist/cli.js' } }, ['src/', 'src/cli.ts']),
+      ).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [{ input: 'src/cli' }],
+         warnings: [],
+      })
+      expect(
+         inferEntries({ bin: { nuxt: 'dist/cli.js' }, type: 'module' }, [
+            'src/',
+            'src/cli.ts',
+         ]),
+      ).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [{ input: 'src/cli' }],
+         warnings: [],
+      })
+   })
 
-  it("matches nested entrypoint paths", () => {
-    const result = inferEntries({ exports: "dist/runtime/index.js" }, [
-      "src/",
-      "src/other/runtime/index.ts",
-    ]);
-    expect(result).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [{ input: "src/other/runtime/index" }],
-      warnings: [],
-    });
-  });
-
-  it("handles declarations from `types`", () => {
-    expect(
-      inferEntries(
-        { main: "dist/test.cjs", types: "custom/handwritten.d.ts" },
-        ["src/", "src/test.ts"]
-      )
-    ).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [{ input: "src/test" }],
-      warnings: ["Could not find entrypoint for custom/handwritten.d.ts"],
-    });
-    expect(
-      inferEntries(
-        {
-          main: "dist/test.cjs",
-          module: "dist/test.mjs",
-          types: "dist/test.d.ts",
-        },
-        ["src/", "src/test.ts"]
-      )
-    ).to.deep.equal({
-      cjs: true,
-      dts: true,
-      entries: [{ input: "src/test" }],
-      warnings: [],
-    });
-    expect(
-      inferEntries(
-        {
-          main: "dist/test.cjs",
-          module: "dist/test.mjs",
-          typings: "dist/test.d.ts",
-        },
-        ["src/", "src/test.ts"]
-      )
-    ).to.deep.equal({
-      cjs: true,
-      dts: true,
-      entries: [{ input: "src/test" }],
-      warnings: [],
-    });
-  });
-
-  it("handles types within exports`", () => {
-    const result = inferEntries(
-      {
-        exports: {
-          types: "dist/test.d.ts",
-          import: "dist/test.mjs",
-          require: "dist/test.cjs",
-        },
-      },
-      ["src/", "src/test.ts"]
-    );
-    expect(result).to.deep.equal({
-      cjs: true,
-      dts: true,
-      entries: [{ input: "src/test" }],
-      warnings: [],
-    });
-  });
-
-  it("gracefully handles unknown entries", () => {
-    expect(
-      inferEntries({ exports: "dist/test.js" }, ["src/", "src/index.ts"])
-    ).to.deep.equal({
-      cjs: false,
-      entries: [],
-      dts: false,
-      warnings: ["Could not find entrypoint for dist/test.js"],
-    });
-  });
-
-  it("ignores top-level exports", () => {
-    expect(
-      inferEntries({ exports: { "./*": "./*" } }, [
-        "src/",
-        "src/",
-        "src/index.ts",
+   it('recognises `type: module` projects', () => {
+      const result = inferEntries({ main: 'dist/test.js', type: 'module' }, [
+         'src/',
+         'src/test.ts',
       ])
-    ).to.deep.equal({
-      cjs: false,
-      entries: [],
-      dts: false,
-      warnings: [],
-    });
-  });
+      expect(result).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [{ input: 'src/test' }],
+         warnings: [],
+      })
+   })
 
-  it("handles multiple entries", () => {
-    expect(
-      inferEntries(
-        {
-          exports: {
-            ".": "./dist/index.cjs",
-            "first-test": "./dist/first-test.cjs",
-            "./test": "./dist/test.cjs",
-          },
-        },
-        ["src/", "src/", "src/index.ts", "src/first-test.ts", "src/test.mjs"]
-      )
-    ).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [
-        { input: "src/index" },
-        { input: "src/first-test" },
-        { input: "src/test" },
-      ],
-      warnings: [],
-    });
-  });
-
-  it("recognises directory mappings", () => {
-    expect(
-      inferEntries({ exports: "./dist/runtime/*" }, [
-        "src/",
-        "src/runtime/",
-        "src/runtime/test.js",
+   it('matches nested entrypoint paths', () => {
+      const result = inferEntries({ exports: 'dist/runtime/index.js' }, [
+         'src/',
+         'src/other/runtime/index.ts',
       ])
-    ).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [
-        { format: "esm", input: "src/runtime/", outDir: "./dist/runtime/" },
-      ],
-      warnings: [],
-    });
-    expect(
-      inferEntries({ exports: { "./runtime/*": "./dist/runtime/*.mjs," } }, [
-        "src/",
-        "src/runtime/",
-      ])
-    ).to.deep.equal({
-      cjs: false,
-      dts: false,
-      entries: [
-        { format: "esm", input: "src/runtime/", outDir: "./dist/runtime/" },
-      ],
-      warnings: [],
-    });
-    expect(
-      inferEntries(
-        { exports: { "./runtime/*": { require: "./dist/runtime/*" } } },
-        ["src/", "src/runtime/"]
-      )
-    ).to.deep.equal({
-      cjs: true,
-      dts: false,
-      entries: [
-        { format: "cjs", input: "src/runtime/", outDir: "./dist/runtime/" },
-      ],
-      warnings: [],
-    });
-  });
-});
+      expect(result).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [{ input: 'src/other/runtime/index' }],
+         warnings: [],
+      })
+   })
 
-describe("getEntrypointPaths", () => {
-  it("produces a list of possible paths", () => {
-    expect(getEntrypointPaths("./dist/foo/bar.js")).to.deep.equal([
-      "dist/foo/bar.js",
-      "foo/bar.js",
-      "bar.js",
-    ]);
-    expect(getEntrypointPaths("./dist/foo/")).to.deep.equal([
-      "dist/foo/",
-      "foo/",
-    ]);
-  });
-});
+   it('handles declarations from `types`', () => {
+      expect(
+         inferEntries(
+            { main: 'dist/test.cjs', types: 'custom/handwritten.d.ts' },
+            ['src/', 'src/test.ts'],
+         ),
+      ).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [{ input: 'src/test' }],
+         warnings: ['Could not find entrypoint for custom/handwritten.d.ts'],
+      })
+      expect(
+         inferEntries(
+            {
+               main: 'dist/test.cjs',
+               module: 'dist/test.mjs',
+               types: 'dist/test.d.ts',
+            },
+            ['src/', 'src/test.ts'],
+         ),
+      ).to.deep.equal({
+         cjs: true,
+         dts: true,
+         entries: [{ input: 'src/test' }],
+         warnings: [],
+      })
+      expect(
+         inferEntries(
+            {
+               main: 'dist/test.cjs',
+               module: 'dist/test.mjs',
+               typings: 'dist/test.d.ts',
+            },
+            ['src/', 'src/test.ts'],
+         ),
+      ).to.deep.equal({
+         cjs: true,
+         dts: true,
+         entries: [{ input: 'src/test' }],
+         warnings: [],
+      })
+   })
+
+   it('handles types within exports`', () => {
+      const result = inferEntries(
+         {
+            exports: {
+               types: 'dist/test.d.ts',
+               import: 'dist/test.mjs',
+               require: 'dist/test.cjs',
+            },
+         },
+         ['src/', 'src/test.ts'],
+      )
+      expect(result).to.deep.equal({
+         cjs: true,
+         dts: true,
+         entries: [{ input: 'src/test' }],
+         warnings: [],
+      })
+   })
+
+   it('gracefully handles unknown entries', () => {
+      expect(
+         inferEntries({ exports: 'dist/test.js' }, ['src/', 'src/index.ts']),
+      ).to.deep.equal({
+         cjs: false,
+         entries: [],
+         dts: false,
+         warnings: ['Could not find entrypoint for dist/test.js'],
+      })
+   })
+
+   it('ignores top-level exports', () => {
+      expect(
+         inferEntries({ exports: { './*': './*' } }, [
+            'src/',
+            'src/',
+            'src/index.ts',
+         ]),
+      ).to.deep.equal({
+         cjs: false,
+         entries: [],
+         dts: false,
+         warnings: [],
+      })
+   })
+
+   it('handles multiple entries', () => {
+      expect(
+         inferEntries(
+            {
+               exports: {
+                  '.': './dist/index.cjs',
+                  'first-test': './dist/first-test.cjs',
+                  './test': './dist/test.cjs',
+               },
+            },
+            ['src/', 'src/', 'src/index.ts', 'src/first-test.ts', 'src/test.mjs'],
+         ),
+      ).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [
+            { input: 'src/index' },
+            { input: 'src/first-test' },
+            { input: 'src/test' },
+         ],
+         warnings: [],
+      })
+   })
+
+   it('recognises directory mappings', () => {
+      expect(
+         inferEntries({ exports: './dist/runtime/*' }, [
+            'src/',
+            'src/runtime/',
+            'src/runtime/test.js',
+         ]),
+      ).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [
+            { format: 'esm', input: 'src/runtime/', outDir: './dist/runtime/' },
+         ],
+         warnings: [],
+      })
+      expect(
+         inferEntries({ exports: { './runtime/*': './dist/runtime/*.mjs,' } }, [
+            'src/',
+            'src/runtime/',
+         ]),
+      ).to.deep.equal({
+         cjs: false,
+         dts: false,
+         entries: [
+            { format: 'esm', input: 'src/runtime/', outDir: './dist/runtime/' },
+         ],
+         warnings: [],
+      })
+      expect(
+         inferEntries(
+            { exports: { './runtime/*': { require: './dist/runtime/*' } } },
+            ['src/', 'src/runtime/'],
+         ),
+      ).to.deep.equal({
+         cjs: true,
+         dts: false,
+         entries: [
+            { format: 'cjs', input: 'src/runtime/', outDir: './dist/runtime/' },
+         ],
+         warnings: [],
+      })
+   })
+})
+
+describe('getEntrypointPaths', () => {
+   it('produces a list of possible paths', () => {
+      expect(getEntrypointPaths('./dist/foo/bar.js')).to.deep.equal([
+         'dist/foo/bar.js',
+         'foo/bar.js',
+         'bar.js',
+      ])
+      expect(getEntrypointPaths('./dist/foo/')).to.deep.equal([
+         'dist/foo/',
+         'foo/',
+      ])
+   })
+})
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,55 +1,55 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from 'vitest'
 import {
-  arrayIncludes,
-  extractExportFilenames,
-  inferExportType,
-} from "../src/utils";
+   arrayIncludes,
+   extractExportFilenames,
+   inferExportType,
+} from '../src/utils'
 
-describe("inferExportType", () => {
-  it("infers export type by condition", () => {
-    expect(inferExportType("import")).to.equal("esm");
-    expect(inferExportType("require")).to.equal("cjs");
-    expect(inferExportType("node")).to.equal("esm");
-    expect(inferExportType("some_unknown_condition")).to.equal("esm");
-  });
-  it("infers export type based on previous conditions", () => {
-    expect(inferExportType("import", ["require"])).to.equal("esm");
-    expect(inferExportType("node", ["require"])).to.equal("cjs");
-    expect(inferExportType("node", ["import"])).to.equal("esm");
-    expect(inferExportType("node", ["unknown", "require"])).to.equal("cjs");
-  });
-});
+describe('inferExportType', () => {
+   it('infers export type by condition', () => {
+      expect(inferExportType('import')).to.equal('esm')
+      expect(inferExportType('require')).to.equal('cjs')
+      expect(inferExportType('node')).to.equal('esm')
+      expect(inferExportType('some_unknown_condition')).to.equal('esm')
+   })
+   it('infers export type based on previous conditions', () => {
+      expect(inferExportType('import', ['require'])).to.equal('esm')
+      expect(inferExportType('node', ['require'])).to.equal('cjs')
+      expect(inferExportType('node', ['import'])).to.equal('esm')
+      expect(inferExportType('node', ['unknown', 'require'])).to.equal('cjs')
+   })
+})
 
-describe("extractExportFilenames", () => {
-  it("handles strings", () => {
-    expect(extractExportFilenames("test")).to.deep.equal([
-      { file: "test", type: "esm" },
-    ]);
-  });
-  it("handles nested objects", () => {
-    expect(extractExportFilenames({ require: "test" })).to.deep.equal([
-      { file: "test", type: "cjs" },
-    ]);
-    // @ts-ignore TODO: fix pkg-types
-    expect(
-      extractExportFilenames({
-        require: { node: "test", other: { import: "this", require: "that" } },
-      })
-    ).to.deep.equal([
-      { file: "test", type: "cjs" },
-      { file: "this", type: "esm" },
-      { file: "that", type: "cjs" },
-    ]);
-  });
-});
+describe('extractExportFilenames', () => {
+   it('handles strings', () => {
+      expect(extractExportFilenames('test')).to.deep.equal([
+         { file: 'test', type: 'esm' },
+      ])
+   })
+   it('handles nested objects', () => {
+      expect(extractExportFilenames({ require: 'test' })).to.deep.equal([
+         { file: 'test', type: 'cjs' },
+      ])
+      expect(
+         extractExportFilenames({
+            // @ts-expect-error is not a valid export type
+            require: { node: 'test', other: { import: 'this', require: 'that' } },
+         }),
+      ).to.deep.equal([
+         { file: 'test', type: 'cjs' },
+         { file: 'this', type: 'esm' },
+         { file: 'that', type: 'cjs' },
+      ])
+   })
+})
 
-describe("arrayIncludes", () => {
-  it("handles strings", () => {
-    expect(arrayIncludes(["test1", "test2"], "test1")).to.eq(true);
-    expect(arrayIncludes(["test1", "test2"], "test3")).to.eq(false);
-  });
-  it("handles regular expressions", () => {
-    expect(arrayIncludes([/t1$/, "test2"], "test1")).to.eq(true);
-    expect(arrayIncludes([/t3$/, "test2"], "test1")).to.eq(false);
-  });
-});
+describe('arrayIncludes', () => {
+   it('handles strings', () => {
+      expect(arrayIncludes(['test1', 'test2'], 'test1')).to.eq(true)
+      expect(arrayIncludes(['test1', 'test2'], 'test3')).to.eq(false)
+   })
+   it('handles regular expressions', () => {
+      expect(arrayIncludes([/t1$/, 'test2'], 'test1')).to.eq(true)
+      expect(arrayIncludes([/t3$/, 'test2'], 'test1')).to.eq(false)
+   })
+})
