8506dd967dd97332ecc47ea2abc6ad9fff932c26
nyxb <contact@nyxb.xyz>
2023-05-13 12:38:57 +0200

    🎉 feat(cli.ts): add CLI entry point to build the project This commit adds a new file `cli.ts` which is the entry point for the CLI tool to build the project. The `main()` function is an async function that parses the command line arguments using the `mri` package and then calls the `build()` function with the root directory and the `stub` argument. If there is an error, it is caught and logged to the console. The `main()` function is then called and any errors are logged to the console and the process exits with a status code of 1.

---
+++ src/cli.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { resolve } from 'nyxpath'
+import mri from 'mri'
+import { build } from './build'
+
+async function main() {
+   const args = mri(process.argv.splice(2))
+   const rootDir = resolve(process.cwd(), args._[0] || '.')
+   await build(rootDir, args.stub).catch((error) => {
+      console.error(`Error building ${rootDir}: ${error}`)
+      throw error
+   })
+}
+
+main().catch((error) => {
+   console.error(error)
+   process.exit(1)
+})
